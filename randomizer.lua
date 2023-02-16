14c88084880f0401080dcbd993749e4144d00b4a
ilzr0 <118184363+ilzr0@users.noreply.github.com>
2023-02-16 15:39:54 -0300

    changed size x2

--- randomizer.lua
+++ randomizer.lua
@@ -5,21 +5,9 @@ local head = character:FindFirstChild("Head")
 if head then
     local ball = Instance.new("Part")
     ball.Shape = Enum.PartType.Ball
-    ball.Size = Vector3.new(2, 2, 2)
+    ball.Size = Vector3.new(10, 10, 10)
     ball.Color = Color3.new(1, 0, 0)
     ball.Position = head.Position + head.CFrame.LookVector * 5
 
     ball.Parent = game.Workspace
-else
-    character.ChildAdded:Connect(function(child)
-        if child.Name == "Head" then
-            local ball = Instance.new("Part")
-            ball.Shape = Enum.PartType.Ball
-            ball.Size = Vector3.new(10, 10, 10)
-            ball.Color = Color3.new(1, 0, 0) -- this is real
-            ball.Position = character.Head.Position + character.Head.CFrame.LookVector * 5
-
-            ball.Parent = game.Workspace
-        end
-    end)
 end
