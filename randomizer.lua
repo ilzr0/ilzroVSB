f1e4c7f945a3e56826b374f7a0a686e1ce64348f
ilzr0 <118184363+ilzr0@users.noreply.github.com>
2023-02-16 15:16:00 -0300

    Update randomizer.lua

--- randomizer.lua
+++ randomizer.lua
@@ -5,6 +5,10 @@ local ball = Instance.new("Part")
 ball.Shape = Enum.PartType.Ball
 ball.Size = Vector3.new(2, 2, 2)
 ball.Color = Color3.new(1, 0, 0)
-ball.Position = character.Head.CFrame:ToWorldSpace(Vector3.new(0, 0, -5))
 
+local offset = Vector3.new(0, 0, -5)
+local headPos = character.Head.CFrame.p
+local headForward = character.Head.CFrame.LookVector
+
+ball.CFrame = CFrame.new(headPos + headForward * offset, headPos + headForward * offset)
 ball.Parent = game.Workspace
