8d18d53f0f5746114b294c44dd9a76a004ad4c81
ilzr0 <118184363+ilzr0@users.noreply.github.com>
2023-02-17 14:16:26 -0300

    da

--- randomizer.lua
+++ randomizer.lua
@@ -15,7 +15,7 @@ if head then
         print("cap")
         local random = math.random(1,1)
         if random == 1 then
-            ball.Color = Color3.fromRGB(175,55,230)
+            ball.Color = Color3:fromRGB(175,55,230)
             local effect = Instance.new("Sparkles")
             effect.Parent = ball
         end
