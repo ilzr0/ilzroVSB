2d0947c71e1b197b97290c87de74442dae7dfe69
ilzr0 <118184363+ilzr0@users.noreply.github.com>
2023-02-17 14:14:57 -0300

    ergegerg

--- randomizer.lua
+++ randomizer.lua
@@ -6,15 +6,16 @@ if head then
     local ball = Instance.new("Part")
     ball.Shape = Enum.PartType.Ball
     ball.Size = Vector3.new(5, 5, 5)
+    ball.Color = Color3.new(1, 0, 0)
     ball.Position = head.Position + head.CFrame.LookVector * 5
-    ball.Color = Color3.new(175,55,230)
+
     local clickDetector = Instance.new("ClickDetector")
     clickDetector.Parent = ball
     clickDetector.MouseClick:Connect(function()
         print("cap")
         local random = math.random(1,1)
         if random == 1 then
-            ball.Color = Color3.new(175,55,230)
+            ball.Color = Color3.fromRGB(175,55,230)
             local effect = Instance.new("Sparkles")
             effect.Parent = ball
         end
