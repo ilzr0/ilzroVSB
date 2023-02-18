0bf6eb063e9ba87ec2042ee0f6fe031da1cfc3a4
ilzr0 <118184363+ilzr0@users.noreply.github.com>
2023-02-18 08:15:22 -0300

    e

--- randomizer.lua
+++ randomizer.lua
@@ -5,8 +5,7 @@ local head = character:FindFirstChild("Head")
 if head then
     local ball = Instance.new("Part")
     ball.Shape = Enum.PartType.Ball
-    ball.Size = Vector3.new(5, 5, 5)
-    ball.Color = Color3.new(1, 0, 0)
+    ball.Size = Vector3.new(4, 4, 4)
     ball.Position = head.Position + head.CFrame.LookVector * 5
 
     local clickDetector = Instance.new("ClickDetector")
